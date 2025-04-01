977ac968b886c5c02122904153bbcd765bad9158
Volodymyr Beimuk <tetrenkuchioan@gmail.com>
2025-04-01 19:19:33 +0300

    Added input and output function definitions

---
+++ app/io/input.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+def read_from_console() -> str:
+    pass
+
+
+def read_from_file(file_path: str) -> list:
+    pass
+
+
+def read_from_file_with_pandas(file_path: str) -> pd.DataFrame:
+    pass
---
+++ app/io/output.py
@@ -0,0 +1,6 @@
+def write_to_console(data: str) -> None:
+    pass
+
+def write_to_file(file_path: str, data: str) -> None:
+    pass
+
